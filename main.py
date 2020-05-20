b0f6370d5ad2c347e8c90d547a6cad207a4baa77
Steve Stoveld <steve@stoveld.xyz>
2020-05-20 17:27:24 -0400

    edits

--- main.py
+++ main.py
@@ -1,22 +1,55 @@
 from selenium import webdriver
+from wordpress_xmlrpc import Client
+from wordpress_xmlrpc import WordPressPost
+from wordpress_xmlrpc.methods import posts
+import creds
 
-DRIVER_PATH = 'chromedriver.exe'
-driver = webdriver.Chrome(executable_path=DRIVER_PATH)
-driver.get('https://3cx.com/blog')
+def grabLatestPost():
+    # define webdriver and blog url
+    DRIVER_PATH = 'chromedriver.exe'
+    driver = webdriver.Chrome(executable_path=DRIVER_PATH)
+    driver.get('https://3cx.com/blog')
 
-content = driver.find_element_by_class_name('fusion-read-more')
-content.click()
+    # search for the read more link and click it
+    content = driver.find_element_by_class_name('fusion-read-more')
+    content.click()
 
-driver.implicitly_wait(10)
-title = driver.find_element_by_class_name('fusion-post-title')
-title = title.get_attribute('outerHTML')
-print(title)
+    # wait for page to load and grab the title
+    driver.implicitly_wait(10)
+    title = driver.find_element_by_class_name('fusion-post-title')
+    title = title.get_attribute('innerHTML')
 
-post = driver.find_element_by_class_name('post-content')
-post = post.get_attribute('outerHTML')
-print(str(post))
+    # grab the content of the post
+    post = driver.find_element_by_class_name('post-content')
+    post = post.get_attribute('outerHTML')
 
-# write to a file
-# f = open("test.html", "w")
-# f.write(post)
-# f.close
+    return post, title
+
+def writeToFile():
+    # set post and encode in utf-8 to handle unicode characters when writing
+    post = grabLatestPost()
+    filename = str(post[1])+'.html'
+    with open(filename, 'wb') as f:
+        f.write(post[0].encode('utf-8'))
+        f.close()
+
+def sendToWP():
+    url = creds.creds['url']
+    user = creds.creds['user']
+    pwd = creds.creds['pwd']
+
+
+    client = Client(url, user, pwd)
+
+    post = grabLatestPost()
+    wppost = WordPressPost()
+    wppost.title = post[1]
+    wppost.content = post[0]
+    wppost.id = client.call(posts.NewPost(wppost))
+
+    wppost.post_status = 'draft'
+    client.call(posts.EditPost(wppost.id, wppost))
+
+
+# writeToFile()
+sendToWP()
